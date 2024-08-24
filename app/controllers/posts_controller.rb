b5bdfe07c27c5f381f8b9954f9d677bec44c54d1
karaage30 <a.yasuharu30@gmail.com>
2024-08-24 14:16:32 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,5 @@
 class PostsController < ApplicationController
 
   def index  # indexアクションを定義した
-    @posts = Post.all  # すべてのレコードを@postsに代入
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(memo: params[:memo])
-  end
+    end
 end
